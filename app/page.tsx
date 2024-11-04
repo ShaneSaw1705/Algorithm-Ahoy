ecd9ec9cee00f2a6322bdb50a8c59e67fb5ec033
shane-curtis <142857801+ShaneSaw1705@users.noreply.github.com>
2024-11-05 12:16:16 +1300

    Added better layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,47 @@
 'use client'
 import { Editor } from "@monaco-editor/react";
-import { useEffect, useState } from "react";
-import init, { new_game, GameState } from "@/public/wasm/wasm";
+import { Key, useEffect, useState } from "react";
+import init, { GameState } from "@/public/wasm/wasm";
 
 export default function Home() {
-
 	const [board, setBoard] = useState<GameState>();
 
 	useEffect(() => {
 		const initilize = async () => {
 			await init();
-			setBoard(new_game(4));
+			setBoard(GameState.new(20))
 		}
 		initilize();
 	}, []);
 
 	return (
-		<div className="grid grid-cols-2 bg-slate-300">
-			<div className="flex flex-col">
-				<div className="flex flex-row gap-2">
+		<div className="flex h-screen bg-slate-300">
+			<div className="flex flex-col flex-grow">
+				<div className="flex flex-row gap-2 p-2">
 					<button className="bg-green-500 text-white px-4 py-2" onClick={() => console.log(board?.get_board())}>Run Code</button>
+					<button className="bg-blue-500 text-white px-4 py-2">Help?</button>
 				</div>
 				<Editor
-					height="100vh"
+					className="flex-grow"
 					defaultLanguage="python"
-					defaultValue="// code"
+					defaultValue="# code"
 				/>
 			</div>
-			<div className="grid grid-rows-2">
-				<h1 className="bg-white text-black">Game Window</h1>
-				<div className="bg-slate-300 text-black p-2">{board?.get_board()}</div>
+			<div className="bg-slate-300 flex items-center justify-center text-black p-2">
+				<div className="w-full h-full flex flex-col justify-center items-center">
+					{board && board.get_board().map((row, rowIndex) => (
+						<div key={rowIndex} className="flex">
+							{row.map((cell: string, colIndex: Key) => (
+								<div
+									key={colIndex}
+									className={`w-8 h-8 border ${cell === 'X' ? 'bg-blue-500' : 'bg-white'}`}
+								>
+								</div>
+							))}
+						</div>
+					))}
+					<div className="w-full h-full text-black">console</div>
+				</div>
 			</div>
 		</div>
 	);
