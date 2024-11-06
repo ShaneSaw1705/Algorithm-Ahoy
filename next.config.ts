2503ad830756cccd3c0d3a2d721a6e7c5a8b45e2
shane-curtis <142857801+ShaneSaw1705@users.noreply.github.com>
2024-11-06 17:51:12 +1300

    still trying to deploy lol

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
+	// Other Next.js configurations
+	webpack: (config, { isServer }) => {
+		if (!isServer) {
+			config.output.webassemblyModuleFilename = "static/wasm/[modulehash].wasm";
+		}
+		return config;
+	},
+	async headers() {
+		return [
+			{
+				source: "/wasm/:path*",
+				headers: [
+					{
+						key: "Content-Type",
+						value: "application/wasm",
+					},
+				],
+			},
+		];
+	},
 };
 
 export default nextConfig;
+
